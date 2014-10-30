0902c346dc40f52188731354d81c1aef9d35fb77
Mike Trexler <info@michaeltrexler.com>
2014-10-30 17:58:49 -0400

    Tutorial 3: Class Based Views Complete

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 - Tutorial 1: Serialization.
 - Tutorial 2: Requests and Responses.
+	- [The Browsable API](http://www.django-rest-framework.org/topics/browsable-api "The Browsable API")
 - Tutorial 3: Class Based Views.
 - Tutorial 4: Authentication and Permissions.
 - Tutorial 5: Relationships and hyperlinked APIs
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,40 +1,12 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
-# We're building our view using GenericAPIView, 
-# and adding in ListModelMixin and CreateModelMixin.
-#
-# The base class provides the core functionality, 
-# and the mixin classes provide the .list() and .create() actions. 
-# We're then explicitly binding the get and post methods to the appropriate actions.
-class SnippetList(mixins.ListModelMixin,
-				  mixins.CreateModelMixin,
-				  generics.GenericAPIView):
+# Generic Class Based Views
+class SnippetList(generics.ListCreateAPIView):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
-
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
-
-# Again we're using the GenericAPIView class to provide the core functionality, 
-# and adding in mixins to provide the .retrieve(), .update() and .destroy() actions.
-class SnippetDetail(mixins.RetreiveModelMixin,
-					mixins.UpdateModelMixin,
-					mixins.DestroyModelMixin,
-					generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
-
-	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
-
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
-	
-	def delete(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)
