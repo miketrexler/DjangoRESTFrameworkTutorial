1ed193852c6cd60ca620ce85474fa6470110035c
Mike Trexler <info@michaeltrexler.com>
2014-11-03 15:58:46 -0500

    Tutorial 6A: Refactor Vies with ViewSets

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,20 +1,39 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
-from snippets import views
+from snippets.views import SnippetViewSet
+from rest_framework import renderers
+
+# We're creating multiple views from each ViewSet class, 
+# by binding the http methods to the required action for each view.
+snippet_list = SnippetViewSet.as_view({
+	'get': 'list',
+	'post': 'create'
+})
+
+snippet_detail = SnippetViewSet.as_view({
+	'get': 'retrieve',
+	'put': 'update',
+	'patch': 'partial_update',
+	'delete': 'destroy'
+})
+
+snippet_highlight = SnippetViewSet.as_view({
+	'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
 
 urlpatterns = patterns('snippets.views',
     url(r'^$', 'api_root'),
 
     url(r'^snippets/$', 
-    	views.SnippetList.as_view(), 
+    	snippet_list, 
     	name='snippet-list'),
 
     url(r'^snippets/(?P<pk>[0-9]+)/$', 
-    	views.SnippetDetail.as_view(),
+    	snippet_detail,
     	name='snippet-detail'),
 
     url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', 
-    	views.SnippetHighlight.as_view(),
+    	snippet_highlight,
     	name='snippet-highlight'),
 )
 
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -6,21 +6,22 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers
+from rest_framework.decorators import detail_route
+from rest_framework import viewsets
 
-# Generic Class Based Views
-class SnippetList(generics.ListCreateAPIView):
+#ViewSet
+class SnippetViewSet(viewsets.ModelViewSet):
+	"""
+	This viewset automatically provides `list`, `create`, `retrieve`,
+	`update` and `destroy` actions.
+
+	Additionally we also provide an extra `highlight` action.
+	"""
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Snippet.objects.all()
-	serializer_class = SnippetSerializer
-
-class SnippetHighlight(generics.GenericAPIView):
-	queryset = Snippet.objects.all()
-	renderer_classes = (renderers.StaticHTMLRenderer,)
-
-	def get(self, request, *args, **kwargs):
+	@detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+	def highlight(self, request, *args, **kwargs):
 		snippet = self.get_object()
 		return Response(snippet.highlighted)
 
